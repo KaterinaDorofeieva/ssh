ad9f7247ff73480dcbcffbd82e6c49ca6da06e86
Katerina Dorofeieva <dorofeievakatya20@gmail.com>
2024-10-22 12:01:20 +0300

    ffefef

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*alert('Go away');*/
+/*const result = confirm('Are u here&');*/
+// const answer = +prompt('Why u here there&', '');
+// console.log(answer + 10);
+
+// const arrAnswers = [];
+// arrAnswers[0] = prompt('do u need help','');
+// arrAnswers[1] = prompt('hello','5');
+// arrAnswers[2] = prompt('fd','');
+
+// document.write(arrAnswers);
+
+const category = 'toys';
+alert(`Hello,${category}`);
+console.log(`hello${category}doll`);
